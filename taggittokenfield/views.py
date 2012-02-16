0785554db32573f3c693ef4d3a6de952a2f841cc
Harris Lapiroff <harris@chromamine.com>
2012-02-16 15:37:22 -0500

    Tag searches should be case-insensitive.

--- taggittokenfield/views.py
+++ taggittokenfield/views.py
@@ -5,7 +5,7 @@ from taggit.models import Tag
 def filter_tags(request):
 	"Returns a JSON formatted list of tags starting with the value of GET variable 'q'."
 	limiter = request.GET['q']
-	results = Tag.objects.filter(name__startswith=limiter).values_list('name', flat=True)
+	results = Tag.objects.filter(name__istartswith=limiter).values_list('name', flat=True)
 	data = []
 	for tag_name in results:
 		data.append({'id': '"%s"' % tag_name, 'name': tag_name})
